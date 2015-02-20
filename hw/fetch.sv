a859d83123ab37fbb7ebe9a1276c54f278520a63
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-02-20 15:30:05 +0000

    Add content

--- hw/fetch.sv
+++ hw/fetch.sv
@@ -5,8 +5,32 @@
 // Description: The first stage in pipleline, 
 // 				fetch the instruction from memory.
 //
-// Vision: Ver 1.0.0 - File Create
+// Vision: Ver 1.0.1 - Add content
 // Comments: 
 //
 ////////////////////////////////////////////////
-module fetch
+module fetch(
+	input i_clk,
+	input [31:0] i_addr_PC,
+	input [31:0] i_data_Instr,
+
+	output o_data_Instr,
+	output o_addr_PC
+	);
+
+//registers in pipeline
+logic [31:0] incremental_PC;
+logic [31:0] fetch_instruction;
+
+//outputs
+assign o_data_Instr = fetch_instruction;
+assign o_addr_PC = incremental_PC;
+
+//store value into pipeline
+always_ff @(posedge clk) 
+begin : store_pipeline
+	incremental_PC <= i_addr_PC;
+	fetch_instruction <= i_data_Instr;
+end
+
+endmodule
