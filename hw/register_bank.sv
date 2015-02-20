0aa812e31db9d370e757c7b3a606762783144814
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-02-20 12:39:04 +0000

    create module files

--- hw/register_bank.sv
+++ hw/register_bank.sv
@@ -1,3 +1,28 @@
+////////////////////////////////////////////////
+// Project: ARC MIPS processor 
+// Designer: Zhiyuan Jiang
+// Module: Register bank
+// Description: 
+//
+// Vision: Ver 1.0.1 - Add register list
+// Comments: 
+//
+////////////////////////////////////////////////
+//Number	Name		Comments
+//	0		$zero		Always zero
+//	1		$at			Reserved for assembler
+// 2-3		$v0-$v1		First and second return values, respectively
+// 4-7		$a0-$a3		First four arguments to functions
+// 8-15		$t0-$t7		Temporary registers
+//16-23		$s0-$s7		Saved registers
+//24-25		$t8-$t9		More temporary registers
+//26-27		$k0-$k1		Reserved for kernel (operating system)
+//	28		$gp			Global pointer
+//	29		$sp			Stack pointer
+//	30		$fp			Frame pointer
+//	31		$ra			Return address
+////////////////////////////////////////////////
+
 module register_bank(
 	input i_clk,
 
