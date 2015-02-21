cc09dc001aabd4c78b9a6bd757f4e350ae74cc53
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-02-21 22:21:39 +0000

    header

--- hw/core.sv
+++ hw/core.sv
@@ -8,78 +8,27 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module core(input logic clk, )
+module core(
+	input logic clk,
+	input logic [31:0] read_instruction,
+	input logic [31:0] read_data,
 
-fetch fetch_inst(
-	.i_clk					(clk),
-	.i_addr_AddRst			(),
-	.i_con_PCSrc			(),
-	.i_data_Instr			(),
-	.o_data_Instr			(),
-	.o_addr_NextPC			(),
-	.o_addr_PC				()
+	output logic [31:0] instruction_address,
+	output logic [31:0] data_address,
+	output logic [31:0] write_data
 	)
 
-decode decode_inst(
-	.i_clk					(clk),
-	.i_con_RegWr			(),
-	.i_addr_NextPC			(),
-	.i_data_Instr			(),
-	.i_addr_WrReg			(),
-	.i_data_WrData			(),
+wire [31:0] add_result;
+wire 
 
-	//control
-	.o_con_ex_regdst		(),
-	.o_con_mem_branch		(),
-	.o_con_mem_memread		(),
-	.o_con_wb_memtoreg		(),
-	.o_con_mem_memwrite		(),
-	.o_con_ex_alusrc		(),
-	.o_con_wb_regwrite		(),
-	.o_con_ex_aluop			(),
-	//Next PC
-	.o_addr_NextPC			(),
-	// registers
-	.o_data_rs				(),
-	.o_data_rt				(),
-	//sign-extend
-	.o_data_SignExt			(),
-	//rd address
-	.o_addr_mux_0			(),
-	.o_addr_mux_1			()
-	)
 
-execute execute_inst(
-	.i_clk					(clk),
-	//control
-	.i_con_ex_regdst		(),	
-	.i_con_mem_branch		(),
-	.i_con_mem_memread		(),
-	.i_con_wb_memtoreg		(),
-	.i_con_mem_memwrite		(),
-	.i_con_ex_alusrc		(),
-	.i_con_wb_regwrite		(),
-	.i_con_ex_aluop			(),
-	//Next PC
-	.i_addr_NextPC			(),
-	// registers
-	.i_data_rs				(),
-	.i_data_rt				(),
-	//sign-extend
-	.i_data_SignExt			(),
-	//rd address
-	.i_addr_mux_0			(),
-	.i_addr_mux_1			(),
-
-	.o_con_mem_branch		(),
-	.o_con_mem_memread		(),
-	.o_con_wb_memtoreg		(),
-	.o_con_mem_memwrite		(),
-	.o_con_wb_regwrite		(),
-
-	.o_data_AddRst			(),
-	.o_con_Zero				(),
-	.o_data_ALU_Rst			(),
-	.o_data_rt				(),
-	.o_addr_MuxRst			()
-	)
+fetch u_fetch(
+	.i_clk(clk),
+	.i_addr_AddRst(add_result),
+	.i_con_PCSrc(),
+	.i_data_Instr,
+	
+	.o_data_Instr,
+	.o_addr_NextPC,
+	.o_addr_PC
+);
