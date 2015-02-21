f9fcde25ecc3419cfcb0529209a52c7221a9a605
shanyueGG <weixiaofamily@sina.com>
2015-02-21 15:33:30 +0000

    Update mux.sv

--- hw/mux.sv
+++ hw/mux.sv
@@ -8,4 +8,16 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module mux
+module mux2 #(parameter WIDTH = 32)
+              (input  [WIDTH-1:0] d0, d1, 
+               input              s, 
+               output [WIDTH-1:0] y);
+
+  assign y = s ? d1 : d0; 
+
+endmodule
+
+mux2 #(32) IF_pcmux (.d0 (i_addr_pcadd4),
+                     .di (i_addr_pcbranchM), 
+                     .s  (i_con_PCSrc), 
+                     .y  (o_addr_nextpc));
