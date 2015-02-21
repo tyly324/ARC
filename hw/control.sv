1c08c820f982bea9c4d8691d000035c71b888902
shanyueGG <weixiaofamily@sina.com>
2015-02-21 20:57:56 +0000

    debug

--- hw/control.sv
+++ hw/control.sv
@@ -9,15 +9,15 @@
 //
 ////////////////////////////////////////////////
 module control
-(output o_con_regdst, o_con_branch, o_con_memread, o_con_memtoreg,  
+(output logic o_con_regdst, o_con_branch, o_con_memread, o_con_memtoreg,  
         o_con_memwrite, o_con_alusrc, o_con_regwrite, 
-        [1:0] o_con_aluop, 
+       logic [1:0] o_con_aluop, 
  input  [5:0] i_con_instru);
 //WB: o_con_memtoreg, o_con_regwrite, 2bits
 //MB: o_con_memread, o_con_memwrite, o_con_branch, 3bits
 //EX: o_con_aluop, o_con_alusrc, o_con_regdst, 4bits
 
-always @ ff(posedge clock)
+always
 begin
   case (i_con_instru)
     001000  :  begin o_con_regdst <= 0;
