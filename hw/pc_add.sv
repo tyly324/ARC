99626141fb2cc87d60f0264108dd5567938b64be
shanyueGG <weixiaofamily@sina.com>
2015-02-21 16:22:24 +0000

    change module name pc_add

---
+++ hw/pc_add.sv
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////
+// Project: ARC MIPS processor 
+// Designer: 
+// Module: 
+// Description: 
+//
+// Vision: Ver 1.0.0 - File Create
+// Comments: 
+//
+////////////////////////////////////////////////
+module pc_add(input [31:0]i_addr_pc, 
+              output [31:0] o_addr_pcadd4);
+      
+    assign o_addr_pcadd4 =i_addr_pc + 32'b100;
+    
+endmodule
+    
