acf8869c0079df9ae9947c3c16d40c6f60bd4965
shanyueGG <weixiaofamily@sina.com>
2015-02-21 14:57:07 +0000

    Update pc.sv

--- hw/pc.sv
+++ hw/pc.sv
@@ -8,4 +8,12 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module pc
+module pc_ff(input clock,
+             input [31:0] next_pc,
+             output[31:0] present_pc);
+
+always_ff(posedge clock)
+      present_pc <= next_pc;
+      
+endmodule
+             
