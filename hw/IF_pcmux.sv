bc72c910e2573cf7b3fc4ce8ce4ff1e1f75bee02
shanyueGG <weixiaofamily@sina.com>
2015-02-21 16:56:55 +0000

    modify mux code

--- hw/IF_pcmux.sv
+++ hw/IF_pcmux.sv
@@ -1,42 +1,10 @@
-////////////////////////////////////////////////
-// Project: ARC MIPS processor 
-// Designer: 
-// Module: 
-// Description: 
-//
-// Vision: Ver 1.0.0 - File Create
-// Comments: 
-//
-////////////////////////////////////////////////
-module mux2 #(parameter WIDTH = 32)
-              (input  [WIDTH-1:0] d0, d1, 
-               input              s, 
-               output [WIDTH-1:0] y);
+module IF_pcmux 
+              (input  [31:0] i_addr_pcadd4, i_addr_pcbranchM, 
+               input         i_con_pcsrc, 
+               output [31:0] o_addr_nextpc);
 
-  assign y = s ? d1 : d0; 
+  assign o_addr_nextpc = i_con_pcsrc ? i_addr_pcbranchM : i_addr_pcadd4; 
 
-endmodule
+endmodule        
 
-mux2 #(32) IF_pcmux (.d0 (i_addr_pcadd4),
-                     .d1 (i_addr_pcbranchM), 
-                     .s  (i_con_pcsrc), 
-                     .y  (o_addr_nextpc));
-                     
-                     
-                     
-mux2 #(32) EX_alumux (.d0 (i_data_writeE),
-                      .d1 (i_data_immE), 
-                     .s  (i_con_alusrc), 
-                     .y  (o_data_scrb));
-                     
-                     
-mux2 #(5)  EX_writemux (.d0 (instruction[20:16]),
-                       .d1  (instruction[15:11]), 
-                       .s  (i_con_regdst), 
-                       .y  (o_data_writeE));                 
-                     
-                     
-mux2 #(32)  WB_resmux (.d0 (i_data_aluresW),
-                       .d1 (i_data_readW), 
-                       .s  (i_con_memtoreg), 
-                       .y  (o_data_result));   
+
--- hw/WB_resmux.sv
+++ hw/WB_resmux.sv
@@ -10,7 +10,7 @@
 ////////////////////////////////////////////////
 module WB_resmux 
               (input  [31:0] i_data_aluresW, i_data_readW, 
-               input              i_con_memtoreg, 
+               input         i_con_memtoreg, 
                output [31:0] o_data_result);
 
   assign o_data_result = i_con_memtoreg ? i_data_readW : i_data_aluresW; 
