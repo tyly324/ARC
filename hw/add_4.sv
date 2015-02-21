980c1a45241e6ab7ff3bb1d98fc1fbf8144d3949
shanyueGG <weixiaofamily@sina.com>
2015-02-21 14:48:05 +0000

    Update add_4.sv

--- hw/add_4.sv
+++ hw/add_4.sv
@@ -8,4 +8,10 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module add_4
+module pc_add(input [31:0] pc, 
+              output [31:0] pcadd4);
+      
+    assign pcadd4 = pc + 32'b100;
+    
+endmodule
+    
