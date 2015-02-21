e0903f2dd075d98e91cbfa3589d4610963fc1b75
shanyueGG <weixiaofamily@sina.com>
2015-02-21 18:45:16 +0000

    modify

--- hw/sign_extend.sv
+++ hw/sign_extend.sv
@@ -14,6 +14,6 @@ module sign_extend(input [15:0] i_data_immD,
                    output reg [31:0] o_data_immD);
                    
 
-assign o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]};
+assign o_data_immD = {{16{i_data_immD[15]}}, i_data_immD[15:0]};
 
 endmodule
