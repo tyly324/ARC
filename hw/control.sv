807010afb0246f17a31354910b35ef3e4f985290
zyzlxyzy <zyzlxyzy@gmail.com>
2015-02-21 14:55:34 +0000

    Update control.sv

--- hw/control.sv
+++ hw/control.sv
@@ -9,3 +9,10 @@
 //
 ////////////////////////////////////////////////
 module control
+(output o_control_regdst, o_control_branch, o_control_memread, o_control_memtoreg, 
+        o_control_memwrite, o_control_alusrc, o_control_regwrite,
+        o_control_aluop[4:0],
+        i_control_instru[5:0),
+//WB: o_control_memtoreg, o_control_regwrite,
+//MB: o_control_memread, o_control_memwrite, o_control_branch,
+//EX: o_control_aluop, o_control_alusrc, o_control_regdst,
