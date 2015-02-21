782e4e58cdc3fd82298234f6843a806c6ccad810
hy7g14 <sghyang3@gmail.com>
2015-02-21 15:13:11 +0000

    alu and alu_control updated

--- hw/alu.sv
+++ hw/alu.sv
@@ -8,4 +8,4 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module alu
+module alu(output logic [31:0] o_data_AluRes, output logic o_con_Zero, input logic [31:0]i_data_A, [31:0]i_data_B, input logic [3:0]i_con_AluCtrl);
--- hw/alu_control.sv
+++ hw/alu_control.sv
@@ -8,4 +8,4 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-moudle alu_control
+module alu_control(output logic [3:0] o_con_AluCtrl, input logic [1:0] i_con_AluOp, input logic [5:0] i_con_FuncCode);
