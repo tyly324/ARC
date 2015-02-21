09890256ebfd6bfae255f43160a7b7f390867431
hy7g14 <sghyang3@gmail.com>
2015-02-21 16:50:57 +0000

    alu and alu_control updated

--- hw/alu.sv
+++ hw/alu.sv
@@ -9,3 +9,18 @@
 //
 ////////////////////////////////////////////////
 module alu(output logic [31:0] o_data_AluRes, output logic o_con_Zero, input logic [31:0]i_data_A, [31:0]i_data_B, input logic [3:0]i_con_AluCtrl);
+
+always_comb
+	begin: COM 
+		assign o_con_Zero = (o_data_AluRes==0);
+		case(i_con_AluCtrl)
+			0:	o_data_AluRes = i_data_A & i_data_B;
+			1:	o_data_AluRes = i_data_A | i_data_B;
+			2:	o_data_AluRes = i_data_A + i_data_B;
+			6:	o_data_AluRes = i_data_A - i_data_B;
+			7:	o_data_AluRes = i_data_A < i_data_B	? 1:0;
+			12:	o_data_AluRes = ~(A|B);
+			default: o_data_AluRes = 0;
+		endcase
+	end
+endmodule
--- hw/alu_control.sv
+++ hw/alu_control.sv
@@ -9,3 +9,17 @@
 //
 ////////////////////////////////////////////////
 module alu_control(output logic [3:0] o_con_AluCtrl, input logic [1:0] i_con_AluOp, input logic [5:0] i_con_FuncCode);
+always_comb
+	begin:COM 
+		case({i_con_AluOp,i_con_FuncCode})
+			00xxxxxx: 	o_con_AluCtrl=2; 	//LW SW / add
+			01xxxxxx:	o_con_AluCtrl=6;	//BEQ /sub
+			10xx0000:	o_con_AluCtrl=2;	//R /add	
+			1xxx0010:	o_con_AluCtrl=6;	//R /sub
+			10xx0100:	o_con_AluCtrl=0;	//R /AND
+			10xx0101:	o_con_AluCtrl=1;	//R /OR
+			1xxx1010:	o_con_AluCtrl=12;	//R /set on less than
+			default:   	o_con_AluCtrl=15; 	//shouldnt happend
+		endcase
+	end
+endmodule
