b8c9017c445c70660ec87f1d85e4efad784b1d3d
shanyueGG <weixiaofamily@sina.com>
2015-02-21 15:42:07 +0000

    Update sign_extand.sv

--- hw/sign_extand.sv
+++ hw/sign_extand.sv
@@ -8,4 +8,14 @@
 // Comments: 
 //
 ////////////////////////////////////////////////
-module sign_extand
+module sign_extand(input [15:0] instruction[15:0],
+                   input i_con_signext,
+                   output [31:0] o_data_immD);
+                   
+always_ff(posedge clock)
+	begin
+		   if (i_con_signext)  o_data_immD <= {{16{instruction[15]}}, instruction[15:0]};
+	     else                o_data_immD <= {16'b0, instruction[15:0]};
+	end
+
+endmodule
