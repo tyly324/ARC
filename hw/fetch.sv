e2a30442cb8ed51f431fae9cb25cf7c39892bcf1
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-02-21 22:31:11 +0000

    debug

--- hw/fetch.sv
+++ hw/fetch.sv
@@ -15,17 +15,17 @@ module fetch(
 	input [31:0] i_con_PCSrc,
 	input [31:0] i_data_Instr,
 
-	output o_data_Instr,
-	output o_addr_NextPC,
-	output o_addr_PC
+	output [31:0] o_data_Instr,
+	output [31:0] o_addr_NextPC,
+	output [31:0] o_addr_PC
 	);
 
 //wire
-wire mux0;
-wire mux1;
-wire pc_in;
-wire pc_out;
-wire add_out;
+wire [31:0] mux0;
+wire [31:0] mux1;
+wire [31:0] pc_in;
+wire [31:0] pc_out;
+wire [31:0] add_out;
 
 //registers in pipeline
 logic [31:0] next_PC;
@@ -43,19 +43,22 @@ begin : store_pipeline
 	fetch_instruction <= i_data_Instr;
 end
 
-IF_pcmux u_mux( .i_addr_pcadd4(mux0),
+IF_pcmux u_mux( 
+		.i_addr_pcadd4(mux0),
 		.i_addr_pcbranchM(mux1),
 		.i_con_pcsrc(i_con_PCSrc),
 		.o_addr_nextpc(pc_in)
 );
 
-pc_add u_pc_add(  .i_addr_pc(pc_out), 
-		  .o_addr_pcadd4(add_out)
+pc_add u_pc_add(  
+		.i_addr_pc(pc_out), 
+		.o_addr_pcadd4(add_out)
 );
 
-pc u_pc(	.clock(i_clk),
-          	.next_pc(pc_in),
-          	.pc(pc_out)
+pc u_pc(	
+		.clock(i_clk),
+        .next_pc(pc_in),
+        .pc(pc_out)
 );
 
 endmodule
