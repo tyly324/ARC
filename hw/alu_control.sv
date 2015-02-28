26117f78c7852243c13fb3a30df84d7ef45dbc4e
hy7g14 <sghyang3@gmail.com>
2015-02-28 15:49:16 +0000

    28/02

--- hw/alu_control.sv
+++ hw/alu_control.sv
@@ -11,6 +11,7 @@
 module alu_control(output logic [3:0] o_con_AluCtrl, input logic [1:0] i_con_AluOp, input logic [5:0] i_con_FuncCode, input logic [3:0] i_con_Other);
 always_comb
 	begin:COM 
+		o_con_AluCtrl = 15;	//shouldnt happend
 		case(i_con_AluOp)
 			2'b00:	o_con_AluCtrl=2; 	//LW SW / add
 			2'b01:	o_con_AluCtrl=6;	//BEQ /sub
@@ -30,9 +31,6 @@ always_comb
 						39:	o_con_AluCtrl=12;	//R /nor
 						///////////////////////////////////////
 						42,43:	o_con_AluCtrl=7;	//R /slt(set on less than) /sltu 
-
-						
-						default:   	o_con_AluCtrl=15; 	//shouldnt happend
 					endcase
 					end
 			2'b11:  begin
@@ -48,7 +46,6 @@ always_comb
                         6:  o_con_AluCtrl=7;    // slti
                     endcase
 			        end
-			default:	o_con_AluCtrl=15;
 		endcase
 	end
 endmodule
