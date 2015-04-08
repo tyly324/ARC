8f4af212da6afdb30e6b875e76395e4a1a4c1982
hy7g14 <sghyang3@gmail.com>
2015-04-08 21:48:19 +0100

    Update E_alu.sv
    alu

--- hw/E_alu.sv
+++ hw/E_alu.sv
@@ -8,22 +8,22 @@ module E_alu(
 
 always_comb
 	begin: COM 
-		o_con_Zero = (o_data_AluRes==0) ? 1:0;
 		o_data_AluRes = 0;
 		case(i_con_AluCtrl)
 			0:	o_data_AluRes = i_data_A & i_data_B; 	//AND
 			1:	o_data_AluRes = i_data_A | i_data_B;	//OR
 			2:	o_data_AluRes = i_data_A + i_data_B;	//add,addu
 			3:	o_data_AluRes = i_data_B <<i_data_shamt;//sll
-			4:	o_data_AluRes = i_data_B >>i_data_shamt;//srl
-			5:	o_data_AluRes = (i_data_A==i_data_B) ? 1:0;//for bne
+			4:	o_data_AluRes = i_data_B >>>i_data_shamt;//srl
+			5:	o_data_AluRes = i_data_B >>i_data_shamt;//sra
+			//5:	o_data_AluRes = (i_data_A==i_data_B) ? 1:0;//for bne
 			6:	o_data_AluRes = i_data_A - i_data_B;	//sub
 			7:	o_data_AluRes = (i_data_A < i_data_B) ? 1:0;// slt, sltu
 			8:	o_data_AluRes = {i_data_B, 16'b0}; 		//I//lui;
 			9: 	o_data_AluRes = i_data_A + 4;			//J//jal;			
 			12:	o_data_AluRes = ~(i_data_A|i_data_B);	//nor
 			13: o_data_AluRes = i_data_A ^ i_data_B;	//xor
-			14: o_data_AluRes = i_data_A;				//jr (architecture modification expected)
+			//14: o_data_AluRes = i_data_A;				//jr (architecture modification expected)
 			
 		endcase
 	end
