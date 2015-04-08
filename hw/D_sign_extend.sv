b7693d46f98d6701232157775c9dfa5b7f24dcc6
shanyueGG <weixiaofamily@sina.com>
2015-04-08 21:32:22 +0100

    Update D_sign_extend.sv

--- hw/D_sign_extend.sv
+++ hw/D_sign_extend.sv
@@ -10,7 +10,7 @@ module D_sign_extend(input [15:0] i_data_immD,
 	begin
 	case(i_data_opcode,)
 	  6'b001111: o_data_immD <= {{i_data_immD[15:0], 16'b0};
-      6'b001000: o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]};
+          6'b001000: o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]};
 	  6'b000000: if((i_data_funct==6'b100000)||(i_data_funct==6'b100010))
 	             o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]}; 
 
