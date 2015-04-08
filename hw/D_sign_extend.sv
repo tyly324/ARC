38fae2ce0d4225f41517593117b77d4bec9eabd8
shanyueGG <weixiaofamily@sina.com>
2015-04-08 20:50:47 +0100

    Update D_sign_extend.sv

--- hw/D_sign_extend.sv
+++ hw/D_sign_extend.sv
@@ -1,8 +1,18 @@
 `timescale 1ns / 1ps
 module D_sign_extend(input [15:0] i_data_immD,
-                   output reg [31:0] o_data_immD);
+                     input      i_con_signext, 
+                     input      i_clk,
+                     output reg [31:0] o_data_immD);
                    
 
-assign o_data_immD = {{16{i_data_immD[15]}}, i_data_immD[15:0]};
+  always_ff @(posedge i_clk)
+	begin
+	
+	   if (i_con_signext)  o_data_immD <= {{16{a[15]}}, a[15:0]};
+	   else                o_data_immD <= {16'b0, a[15:0]};
+	end
 
 endmodule
+
+
+
