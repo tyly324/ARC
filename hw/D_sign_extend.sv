2ba1430e1861a16f1006ab1cf25c7fbd58984c64
shanyueGG <weixiaofamily@sina.com>
2015-04-09 16:14:45 +0100

    signext

--- hw/D_sign_extend.sv
+++ hw/D_sign_extend.sv
@@ -1,21 +1,18 @@
 `timescale 1ns / 1ps
 module D_sign_extend(input [15:0] i_data_immD,
-                     input [5:0]  i_data_opcode, 
-                     input [5:0]  i_data_funct,
-                     input        i_clk,
+                     input   i_con_signext,
+                     input   i_clk,
                      output reg [31:0] o_data_immD);
                    
 
   always_ff @(posedge i_clk)
 	begin
-	case(i_data_opcode,)
-	  6'b001111: o_data_immD <= {{i_data_immD[15:0], 16'b0};
-          6'b001000: o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]};
-	  6'b000000: if((i_data_funct==6'b100000)||(i_data_funct==6'b100010))
-	             o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]}; 
+       
+      if(i_con_signext)
+       o_data_immD <= {{16{i_data_immD[15]}}, i_data_immD[15:0]};
+      else
+       o_data_immD <= {16'b0, i_data_immD[15:0]};
 
-	  default:   o_data_immD <= {16'b0, i_data_immD[15:0]};
-     endcase
 	end
 
 endmodule
