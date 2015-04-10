286a487e58aaf5356035cdf4e8b561164c6363db
shanyueGG <weixiaofamily@sina.com>
2015-04-10 20:15:37 +0100

    sign

--- hw/E_fbmux3.sv
+++ hw/E_fbmux3.sv
@@ -1,5 +1,6 @@
-`timescale 1ns / 1ps
-module E_fbmux3
+  
+ `timescale 1ns / 1ps
+module W_loadmux4
               (input  logic[31:0] i_data_rt, i_data_alures, i_data_writeres,
                input  logic[1:0]  i_con_fb, 
                output logic[31:0] o_data_alusrb);
