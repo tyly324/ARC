45f6d3deb1bfd41d2bd50486654e78d2f77b0bc1
hy7g14 <sghyang3@gmail.com>
2015-04-17 18:06:48 +0100

    sub

--- hw/E_alu.sv
+++ hw/E_alu.sv
@@ -8,13 +8,19 @@ module E_alu(
 
 logic [16:0] half_sum;
 logic [15:0] pre_suma;
-logic [15:0] pre_sumb; 
+logic [15:0] pre_sumb;
+logic [16:0] half_sub;
+logic [15:0] pre_suba;
+logic [15:0] pre_subb; 
 
 
 assign half_sum = i_data_A[15:0] + i_data_B[15:0];
 assign pre_suma = i_data_A[31:16]+ i_data_B[31:16];
 assign pre_sumb = i_data_A[31:16]+ i_data_B[31:16]+ 1'b1;
 
+assign half_sub = i_data_A[15:0] - i_data_B[15:0];
+assign pre_suba = i_data_A[31:16]- i_data_B[31:16];
+assign pre_subb = i_data_A[31:16]- i_data_B[31:16]- 1'b1;
 always_comb
 	begin: COM 
 		o_data_AluRes = 0;
@@ -32,7 +38,10 @@ always_comb
 			4:	o_data_AluRes = i_data_B >>>i_data_shamt;//srl
 			5:	o_data_AluRes = i_data_B >>i_data_shamt;//sra
 			//5:	o_data_AluRes = (i_data_A==i_data_B) ? 1:0;//for bne
-			6:	o_data_AluRes = i_data_A - i_data_B;	//sub
+			6:	begin					
+					o_data_AluRes [31:16]= half_sub[16] ? pre_subb : pre_suba;//sub, subu
+					o_data_AluRes [15:0] = half_sub[15:0];
+				end
 			7:	o_data_AluRes = (i_data_A < i_data_B) ? 1:0;// slt, sltu
 			8:	o_data_AluRes = {i_data_B, 16'b0}; 		//I//lui;
 			9: 	o_data_AluRes = i_data_A + 4;			//J//jal;			
